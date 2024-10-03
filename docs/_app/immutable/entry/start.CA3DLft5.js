40d7c7addd77ac2a9d78e32d625fccd50ed2533e
JulianTsugami <julian.tsugami@icloud.com>
2024-10-04 00:30:52 +0900

    feat: fill experience section

--- docs/_app/immutable/entry/start.CA3DLft5.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Dw3cWD_k.js";export{t as start};
--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -1,8 +1,62 @@
 <h4 class="pico-color-indigo" id="experience">Experience</h4>
-<small class="pico-color-indigo-950"
-  >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum.
-</small>
+<article style="background-color: #f8f8f8;">
+  <small class="pico-color-indigo"><b>Rakuten Group, Inc</b> - 2023/04 - Present</small>
+  <div style="margin-bottom: 15px;">
+    <small class="pico-color-indigo-950"><i>Associate Application Engineer,</i></small>
+    <br />
+    <small class="pico-color-indigo-950"><i>Travel Development Department,</i></small>
+    <br />
+    <small class="pico-color-indigo-950"><i>Travel Service Reliability Section</i></small>
+  </div>
+  <small class="pico-color-indigo-950">
+    • Led life cycle management projects, updating Java, Spring Framework, and other dependencies
+    for backend applications, and successfully migrated them to new Linux-based on-premise servers,
+    ensuring system compatibility and stability.</small
+  >
+  <br />
+  <small class="pico-color-indigo-950">
+    • Collaborated with QA teams to reproduce, diagnose, and resolve bugs in staging and production
+    environments, implementing thorough regression testing to prevent reoccurrence.
+  </small>
+  <br />
+  <small class="pico-color-indigo-950">
+    • Designed and executed performance tests using Locust and JMeter, identifying and resolving
+    performance bottlenecks, leading to improved application speed and reliability.
+  </small>
+  <br />
+  <small class="pico-color-indigo-950">
+    • Built a Jenkins pipeline for automating the build process of Gradle-based Java repositories,
+    improving CI/ CD efficiency.
+  </small>
+  <br />
+</article>
+<article style="background-color: #f8f8f8;">
+  <small class="pico-color-indigo"><b>ITANDI Inc.</b> - 2021/08 - 2023/01</small>
+  <div style="margin-bottom: 10px;">
+    <small class="pico-color-indigo-950"><i>Data Engineer (Part Time),</i></small>
+    <br />
+    <small class="pico-color-indigo-950"><i>Property Integration Team</i></small>
+    <br />
+    <small class="pico-color-indigo-950"><i>Travel Service Reliability Section</i></small>
+  </div>
+
+  <small class="pico-color-indigo-950">
+    • Developed and maintained the core data pipelines using Ruby and DigDag, ensuring reliable data
+    flow and scalability for critical systems.</small
+  >
+  <br />
+  <small class="pico-color-indigo-950">
+    • Improved the quality of ETL systems by detecting transformation failures via Amazon CloudWatch
+    and implementing solutions to make unprocessed data transformable, increasing data accuracy.
+  </small>
+  <br />
+  <small class="pico-color-indigo-950">
+    • Developed a Python-based web crawler, leveraging Pydantic for data validation, which ensured
+    the accuracy and integrity of collected data, reducing processing errors.
+  </small>
+  <br />
+  <small class="pico-color-indigo-950">
+    • Led four interns in the team, assigning tasks, managing progress, reviewing deliverables, and
+    ensuring timely project completion.
+  </small>
+</article>
